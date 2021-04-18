e86e4e623d4d7903e18375084d81108c0e5560fa
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-04-17 23:30:34 -0300

    Create Job.js

---
+++ src/model/Job.js
@@ -0,0 +1,28 @@
+let data = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 5,
+        "total-hours": 6,
+        created_at: Date.now()
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 6,
+        "total-hours": 56,
+        created_at: Date.now()
+    }
+];
+
+module.exports = {
+    get() {
+        return data;
+    },
+    set(newData) {
+        data = newData;
+    },
+    delete(id) {
+        data = data.filter(job => Number(job.id) !== Number(id));
+    }
+};
