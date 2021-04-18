895f8c850f88f89441b222d84fda072495b70b7d
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-04-18 14:05:26 -0300

    Update config.js

--- src/database/config.js
+++ src/database/config.js
@@ -1,9 +1,7 @@
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 
-module.exports = () => {
-    open({
-        filename: "./src/database/database.sqlite",
-        driver: sqlite3.Database
-    });
-};
+module.exports = () => open({
+    filename: "./src/database/database.sqlite",
+    driver: sqlite3.Database
+});
